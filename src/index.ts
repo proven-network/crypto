6cfd2a05f37624811f80f48f07721c60a317fa7b
flightofthefox <foxy@borderlinelabs.com>
2025-01-15 20:34:37 +0800

    Implement Radix key interfaces for RET compat.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,60 @@
-class VerifyingKey {
+import {
+  Curve as RadixCurve,
+  PublicKey as RadixPublicKey,
+  PrivateKey as RadixPrivateKey,
+  Signer as RadixSigner,
+  SignerResponse as RadixSignerResponse,
+  Signature as RadixSignature,
+  SignatureWithPublicKey as RadixSignatureWithPublicKey,
+} from "@radixdlt/radix-engine-toolkit";
+
+class PublicKey implements RadixPublicKey {
   bytes: Uint8Array;
-  rawBytes: Uint8Array;
+
+  get curve(): RadixCurve {
+    return "Ed25519";
+  }
+
+  get publicKey() {
+    return this.bytes;
+  }
 
   constructor(bytes: Uint8Array) {
     this.bytes = bytes;
-    this.rawBytes = bytes;
+  }
+
+  hex = this.hexString;
+
+  hexString() {
+    return Array.from(this.bytes)
+      .map((byte) => byte.toString(16).padStart(2, "0"))
+      .join("");
+  }
+
+  rawBytes() {
+    return this.bytes;
+  }
+
+  toString = this.hexString;
+}
+
+class Signature implements RadixSignature {
+  bytes: Uint8Array;
+
+  get curve(): RadixCurve {
+    return "Ed25519";
+  }
+
+  get signature(): Uint8Array {
+    return this.bytes;
+  }
+
+  constructor(bytes: Uint8Array) {
+    this.bytes = bytes;
+  }
+
+  hex() {
+    return this.hexString();
   }
 
   hexString() {
@@ -13,16 +63,32 @@ class VerifyingKey {
       .join("");
   }
 
+  rawBytes() {
+    return this.bytes;
+  }
+
   toString = this.hexString;
 }
 
-class Signature {
+class SignatureWithPublicKey implements RadixSignatureWithPublicKey {
   bytes: Uint8Array;
-  rawBytes: Uint8Array;
+  publicKey: Uint8Array;
 
-  constructor(bytes: Uint8Array) {
+  get curve(): RadixCurve {
+    return "Ed25519";
+  }
+
+  get signature(): Uint8Array {
+    return this.bytes;
+  }
+
+  constructor(bytes: Uint8Array, publicKey: Uint8Array) {
     this.bytes = bytes;
-    this.rawBytes = bytes;
+    this.publicKey = publicKey;
+  }
+
+  hex() {
+    return this.hexString();
   }
 
   hexString() {
@@ -31,25 +97,59 @@ class Signature {
       .join("");
   }
 
+  rawBytes() {
+    return this.bytes;
+  }
+
   toString = this.hexString;
 }
 
-export class SigningKey {
+export class PrivateKey implements Omit<RadixPrivateKey, "bytes">, RadixSigner {
   keyId: number;
+
+  get curve(): RadixCurve {
+    return "Ed25519";
+  }
 
   constructor(keyId: number) {
     this.keyId = keyId;
   }
 
-  verifyingKey(): VerifyingKey {
-    return new VerifyingKey(new Uint8Array());
+  produceSignature(messageHash: Uint8Array): RadixSignerResponse {
+    return {
+      curve: this.curve,
+      publicKey: this.publicKeyBytes(),
+      signature: this.sign(messageHash),
+    };
   }
 
-  sign(data: string | Uint8Array): Signature {
+  publicKey(): RadixPublicKey {
+    return new PublicKey(new Uint8Array());
+  }
+
+  publicKeyBytes(): Uint8Array {
+    return this.publicKey().bytes;
+  }
+
+  publicKeyHex(): string {
+    return this.publicKey().hexString();
+  }
+
+  sign(data: string | Uint8Array): Uint8Array {
+    return new Signature(new Uint8Array()).bytes;
+  }
+
+  signToSignature(data: string | Uint8Array): Signature {
     return new Signature(new Uint8Array());
+  }
+
+  signToSignatureWithPublicKey(
+    data: string | Uint8Array
+  ): SignatureWithPublicKey {
+    return new SignatureWithPublicKey(new Uint8Array(), this.publicKeyBytes());
   }
 }
 
 export function generateEd25519Key() {
-  return new SigningKey(0);
+  return new PrivateKey(0);
 }
